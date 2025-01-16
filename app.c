493399c74c265184875d77c70214d1b9a57deb58
ibukye <125569824+ibukye@users.noreply.github.com>
2025-01-16 18:30:21 +0800

    initial commit

---
+++ app.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int get(int number);
+char display_board(char *);
+
+
+int main() {
+    char board[3][3] = {0};
+
+    display(board);
+
+}
+
+
+char display_board(char *board) {
+    for ()printf("")
+}
